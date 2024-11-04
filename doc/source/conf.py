9030fb9193f2c8e2e67eca25c7c7c9edc527c09b
NikitinaMaria <71255897+NikitinaMaria@users.noreply.github.com>
2024-11-04 20:42:17 +0300

    Update conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -15,7 +15,7 @@ import sys
 
 sys.path.insert(0, os.path.abspath('../../src/'))
 
-from mylib import __version__
+from irt import __version__
 
 
 # -- Project information -----------------------------------------------------
